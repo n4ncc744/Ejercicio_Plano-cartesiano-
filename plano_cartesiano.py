cb8d8b11279cefbe204835e5b2774d96eeca8b48
n4ncc744 <nancy2224510@correo.uis.edu.co>
2022-04-23 21:51:46 -0500

    Ejercicio desarrollo

---
+++ plano_cartesiano.py
@@ -0,0 +1,30 @@
+"""Ejercicio del plano cartesiano"""
+
+print("---------------------------------------------")
+print("-----Coordenadas, ejes,origen, y coordenada--")
+print("---------------------------------------------")
+
+#input
+X = int(input("Digite el valor de x: "))
+Y = int(input("Digite el valor de y: "))
+
+#processing
+
+if X > 0 and Y > 0:
+    msj = ("Esta en el primer cuadrante")
+elif X < 0 and  Y  < 0:
+    msj = ("Esta en el segundo cuadrante")
+elif X < 0 and Y > 0:
+    msj = ("Esta en el  cuadrante")
+elif X > 0 and Y < 0: 
+    msj = ("Esta en el cuarto cuadrante")
+elif X == 0 and Y != 0:
+    msj = ("se encuentra en el eje Y")
+elif X != 0 and Y == 0:
+    msj = ("se encuentra en el eje X")
+else:
+    msj = ("esta en el origen")
+
+# output 
+
+print("\nLa coordenada " + msj + "\n ")
